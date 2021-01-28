1d4435f2d1d18a29c2d9defe16b94d0bb2cebd72
taketakeyyy <taketakeyyy@gmail.com>
2021-01-28 14:45:44 +0900

    update: O(log n)になってなかったので修正。関数名も変更。

--- Mod/mod_pow.cpp
+++ Mod/mod_pow.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 
-long long repeat_square(long long a, long long n, long long m) {
+/***
+ * a^{n} mod m の計算を、繰り返し二乗法で求めるライブラリ
+ ***/
+
+
+long long mod_pow(long long a, long long n, long long m) {
     /** a^{n} mod m の計算を、繰り返し二乗法で求める
      *  計算量：O(log n)
      **/
     if (n==0) return 1;
-    if (!(n&1)) {  // nが奇数のとき
-        long long t = repeat_square(a, n/2, m);
-        return (t*t)%m;
-    }
-    // nが偶数のとき
-    return a * repeat_square(a, n-1, m);
+    long long res = mod_pow(a*a%m, n/2, m);
+    if (n&1) res = res * a % m;
+    return res;
 }
 
 
@@ -21,12 +23,12 @@ void test() {
     long long a, n, m;
 
     a = 2LL; n = 20LL; m = pow(10, 9)+7;
-    ans = repeat_square(a, n, m);
+    ans = mod_pow(a, n, m);
     cout << ans << endl;
     // 1048576
 
     a = 2LL; n = 1000000000LL; m = pow(10, 9)+7;
-    ans = repeat_square(a, n, m);
+    ans = mod_pow(a, n, m);
     cout << ans << endl;
     // 140625001
 }
