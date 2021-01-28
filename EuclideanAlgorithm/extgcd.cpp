2a6eaa4a77a1993f57bc5383392ae61ee8f8c807
taketakeyyy <taketakeyyy@gmail.com>
2021-01-28 15:10:08 +0900

    add extgcd.cpp追加 update コメント修正  Please enter the commit message for your changes. Lines starting

---
+++ EuclideanAlgorithm/extgcd.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/***
+ * 拡張ユークリッドの互除法のライブラリ
+ *
+ * 拡張ユークリッドの互除法では、「ax + by = gcd(a,b) となる整数x, yを求める」
+ **/
+
+long long extgcd(long long a, long long b, long long& x, long long& y) {
+    /*** 拡張ユークリッドの互除法
+     * Descriptions
+     *  ax + by = gcd(a,b) となる整数 x, y を求める
+     *
+     * Returns
+     *  gcd(a, b)を返す
+     **/
+    long long d = a;
+    if (b!=0LL) {
+        d = extgcd(b, a%b, y, x);
+        y -= (a/b)*x;
+    }
+    else {
+        x = 1LL; y = 0LL;
+    }
+    return d;
+}
+
+void test(){
+    long long a, b, x, y, g;
+
+    a=111LL; b=30LL;
+    g = extgcd(a, b, x, y);
+    cout << "gcd(a,b)=" << g << ", x=" << x << ", y=" << y << endl;
+    // gcd(a,b)=3, x=3, y=-11
+
+    a=30LL; b=111LL;
+    g = extgcd(a, b, x, y);
+    cout << "gcd(a,b)=" << g << ", x=" << x << ", y=" << y << endl;
+    // gcd(a,b)=3, x=-11, y=3
+
+    a=11LL; b=4LL;
+    g = extgcd(a, b, x, y);
+    cout << "gcd(a,b)=" << g << ", x=" << x << ", y=" << y << endl;
+    // gcd(a,b)=1, x=-1, y=3
+
+    a=4LL; b=11LL;
+    g = extgcd(a, b, x, y);
+    cout << "gcd(a,b)=" << g << ", x=" << x << ", y=" << y << endl;
+    // gcd(a,b)=1, x=3, y=-1
+
+    /* [問題] 111x + 30y = 12となる整数x,yを求めよ */
+    a, b = 111LL, 30LL;
+    g = extgcd(a, b, x, y);
+    // g = 3より、
+    // 111x + 30y = 3の整数解が x=3, y=-11
+    // 求めたい解は、両辺4倍すればよい。よって4x,4yが答え
+}
+
+
+int main(int argc, char const *argv[]){
+    test();
+    return 0;
+}
