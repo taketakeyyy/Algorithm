3e6897c870065afa0d147665f1e2b25c7b3d718c
taketakeyyy <taketakeyyy@gmail.com>
2022-06-24 13:58:26 +0900

    template化

--- Math/gauss_jordan.cpp
+++ Math/gauss_jordan.cpp
@@ -1,37 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-using ll = long long;
-const double EPS = 1E-8;
-using vec = vector<double>;
-using mat = vector<vec>;
+// using ll = long long;
+// using vec = vector<double>;
+// using mat = vector<vec>;
 
 /***
  * Gauss-Jordanの消去法
  *  計算量はO(n^3) （nは式の数）
  ***/
-vec gauss_jordan(const mat& A, const vec& b) {
+template<typename T>
+vector<T> gauss_jordan(const vector<vector<T>>& A, const vector<T>& b) {
     /***
      * Ax=bを解く
      * Aは正方行列
      *   - つまり、式の数と変数の数は同数にすること
      * 解が存在しないか、一意でない場合は長さ0の配列を返す
      **/
-    ll n = A.size();
-    mat B(n, vec(n+1));
-    for (ll i=0; i<n; i++) {
-        for (ll a=0; a<n; a++) {
+    const double EPS = 1E-8;
+    size_t n = A.size();
+    vector<vector<T>> B(n, vector<T>(n+1));
+    for (size_t i=0; i<n; i++) {
+        for (size_t a=0; a<n; a++) {
             B[i][a] = A[i][a];
         }
     }
     // 行列Aの後ろにbを並べて同時に処理する
-    for (ll i=0; i<n; i++) {
+    for (size_t i=0; i<n; i++) {
         B[i][n] = b[i];
     }
 
-    for (ll a=0; a<n; a++) {
+    for (size_t a=0; a<n; a++) {
         // 注目している変数の係数の絶対値が大きい式をa番目に持ってくる
-        ll pivot = a;
-        for (ll j=a; j<n; j++) {
+        size_t pivot = a;
+        for (size_t j=a; j<n; j++) {
             if (abs(B[j][a]) > abs(B[pivot][a])) {
                 pivot = j;
             }
@@ -39,21 +40,21 @@ vec gauss_jordan(const mat& A, const vec& b) {
         swap<double>(B[a], B[pivot]);
 
         // 解がないか、一意でない
-        if (abs(B[a][a]) < EPS) return vec();
+        if (abs(B[a][a]) < EPS) return vector<T>();
 
         // 注目している変数の係数を1にする
-        for (ll j=a+1; j<=n; j++) B[a][j] /= B[a][a];
-        for (ll j=0; j<n; j++) {
+        for (size_t j=a+1; j<=n; j++) B[a][j] /= B[a][a];
+        for (size_t j=0; j<n; j++) {
             if (a==j) continue;
             // j番目の式からa番目の変数を消去する
-            for (ll k=a+1; k<=n; k++) {
+            for (size_t k=a+1; k<=n; k++) {
                 B[j][k] -= B[j][a] * B[a][k];
             }
         }
     }
-    vec x(n);
+    vector<double> x(n);
     // 後ろに並べたbが解になる
-    for (ll i=0; i<n; i++) x[i] = B[i][n];
+    for (size_t i=0; i<n; i++) x[i] = B[i][n];
     return x;
 }
 
@@ -68,16 +69,16 @@ void test(){
 
     // Ax=bを解きたいので、Aとbを作成する
     int n = 3;
-    mat A(n, vec(n));  // Aは正方行列
+    vector<vector<double>> A(n, vector<double>(n));  // Aは正方行列
     A[0][0]=1; A[0][1]=-2; A[0][2]=3;
     A[1][0]=4; A[1][1]=-5; A[1][2]=6;
     A[2][0]=7; A[2][1]=-8; A[2][2]=10;
 
-    vec b(n);
+    vector<double> b(n);
     b[0]=6; b[1]=12; b[2]=21;
 
-    vec ans;
-    ans = gauss_jordan(A, b);
+    vector<double> ans;
+    ans = gauss_jordan<double>(A, b);
 
     // 出力
     if (ans.size()==0) {
