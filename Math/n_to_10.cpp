d253dcc8a8ef814baf57c9ae90411fcfbd318bab
taketakeyyy <taketakeyyy@gmail.com>
2022-06-24 15:06:27 +0900

    template<typename T>化

--- Math/n_to_10.cpp
+++ Math/n_to_10.cpp
@@ -5,24 +5,25 @@ using ll = long long;
 /*** n進数で表現された文字列を、10進数の値にして返すライブラリ
  *   ※ ただしSに使っていい文字は'0'~'9'までの文字
  ***/
-
-ll n_to_10(const ll n, const string& S) {
+template<typename T>
+T n_to_10(const T n, const string& S) {
     // n進数で表現された文字列Sを、10進数の値にして返す
-    ll base = 1;
-    ll v = 0;  // 10進数の値
+    T base = 1;
+    T v = 0;  // 10進数の値
     for(int i=S.size()-1; i>=0; i--) {
-        ll a = S[i]-'0';
+        T a = S[i]-'0';
         v += a*base;
         base *= n;
     }
     return v;
 }
 
-ll n_to_10_snuke(const ll n, const string& S) {
+template<typename T>
+T n_to_10_snuke(const T n, const string& S) {
     // snukeさんがやってた方法
-    ll v = 0;
+    T v = 0;
     for (char c: S) {
-        v = v*n + (ll)c-'0';
+        v = v*n + (T)c-'0';
     }
     return v;
 }
@@ -33,7 +34,7 @@ void test(){
     ll n = 2;
     string S = "101101101"s;
     cout << S << endl;
-    ll ans = n_to_10(n, S);
+    ll ans = n_to_10<ll>(n, S);
     cout << ans << endl;
     // 365
 }
@@ -44,7 +45,7 @@ void test2(){
     ll n = 8;
     string S = "555"s;
     cout << S << endl;
-    ll ans = n_to_10(n, S);
+    ll ans = n_to_10<ll>(n, S);
     cout << ans << endl;
     // 365
 }
@@ -55,7 +56,7 @@ void test3(){
     ll n = 16;
     string S = "123"s;
     cout << S << endl;
-    ll ans = n_to_10(n, S);
+    ll ans = n_to_10<ll>(n, S);
     cout << ans << endl;
     // 291
 }
@@ -67,7 +68,7 @@ void test_snuke(){
     ll n = 2;
     string S = "101101101"s;
     cout << S << endl;
-    ll ans = n_to_10_snuke(n, S);
+    ll ans = n_to_10_snuke<ll>(n, S);
     cout << ans << endl;
     // 365
 }
@@ -78,7 +79,7 @@ void test2_snuke(){
     ll n = 8;
     string S = "555"s;
     cout << S << endl;
-    ll ans = n_to_10_snuke(n, S);
+    ll ans = n_to_10_snuke<ll>(n, S);
     cout << ans << endl;
     // 365
 }
@@ -89,13 +90,13 @@ void test3_snuke(){
     ll n = 16;
     string S = "123"s;
     cout << S << endl;
-    ll ans = n_to_10_snuke(n, S);
+    ll ans = n_to_10_snuke<ll>(n, S);
     cout << ans << endl;
     // 291
 }
 
 
-int main(int argc, char const *argv[]){
+int main() {
     test();
     test2();
     test3();
