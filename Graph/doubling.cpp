d44ae136ebdd66ae221e18d3f165a99b87c099fd
taketakeyyy <taketakeyyy@gmail.com>
2024-09-11 16:04:57 +0900

    fix: ダブリングをクラス化した

--- Graph/doubling.cpp
+++ Graph/doubling.cpp
@@ -12,42 +12,66 @@ const long long INF = LONG_LONG_MAX - 1001001001001001;
 void chmax(int& x, int y) { x = max(x,y); }
 void chmin(int& x, int y) { x = min(x,y); }
 
+
 /**
- * @brief ダブリングの作成
+ * @brief ダブリング
  *
- * @param N T グラフの頂点数
- * @param D T ダブリングの個数。2^60あれば十分なら、D=60
- * @param A vector<T> A[i] := 頂点iの遷移先
- * @return vector<vector<T>>
+ * @tparam T int, long long
  *
  * @example
- *  // ダブリングのグラフ構築
- *  ll D = 60;
- *  vector doubling = make_doubling<ll>(N, D, A);
+ *   // ダブリング作成
+ *   const ll D = 64;
+ *   Doubling<ll> dbl(N, D, G);
  *
- *  // ダブリングを使ってK回の遷移先を求める
- *  ll now = 0;  // 開始の頂点番号
- *  for(ll d=D-1; d>=0; d--) {
- *      if ((K>>d)&1) {
- *          now = doubling[d][now];
- *      }
- *  }
- *  cout << now << endl;  // K回の遷移後の頂点番号
+ *   // 頂点0からK回辿った先の頂点は？
+ *   cout << dbl.query(0, K) << endl;
  */
 template<typename T>
-vector<vector<T>> make_doubling(T N, T D, vector<T> const &A) {
-    // ダブリングのグラフ構築
-    vector doubling(D, vector<ll>(N));  // doubling[d][i] := 頂点iから2^d辿った先の頂点は？
-    for(ll i=0; i<N; i++) {
-        doubling[0][i] = A[i];
-    }
-    for(ll d=1; d<D; d++) {
+class Doubling {
+private:
+
+public:
+    T mN, mD;
+    vector<T> mG;
+    vector<vector<T>> m_doubling; // m_doubling[u][d] := 頂点uから2^d辿った先の頂点
+
+    /**
+     * @brief コンストラクタ
+     * 計算量O(ND)
+     *
+     * @param N 頂点数
+     * @param D 2^Dまで辿る
+     * @param G G[u] := 頂点uの遷移先。Functional Graphであること
+     */
+    Doubling(T N, T D, const vector<T> &G): mN(N), mD(D), mG(G), m_doubling(mN, vector<T>(mD, -1)) {
+        // ダブリングのグラフ構築
         for(ll i=0; i<N; i++) {
-            doubling[d][i] = doubling[d-1][doubling[d-1][i]];  // 2^{d}辿った先は、「2^{d-1}辿った先」を2回辿ればよい
+            m_doubling[i][0] = mG[i];
+        }
+        for(ll d=1; d<D; d++) {
+            for(ll i=0; i<N; i++) {
+                m_doubling[i][d] = m_doubling[m_doubling[i][d-1]][d-1]; // 2^{d}辿った先は、「2^{d-1}辿った先」を2回辿ればよい
+            }
         }
     }
-    return doubling;
-}
+
+    /**
+     * @brief 「頂点uをk回辿る」クエリに応答する
+     * 計算量O(D)
+     *
+     * @param u 頂点u
+     * @param k 頂点uをk回辿る
+     * @return T 頂点uをk回辿ったときの頂点
+     */
+    T query(T u, T k) {
+        ll now = u;
+        for(ll d=mD-1; d>=0; d--) {
+            if ((k>>d)&1) now = m_doubling[now][d];
+        }
+        return now;
+    }
+};
+
 
 void test1() {
     // https://atcoder.jp/contests/abc167/tasks/abc167_d
@@ -59,18 +83,12 @@ void test1() {
     A[2] = 3;
     A[3] = 0;
 
-    // ダブリングのグラフ構築
-    ll D = 60;
-    vector doubling = make_doubling<ll>(N, D, A);
+    // ダブリング作成
+    const ll D = 64;
+    Doubling<ll> dbl(N, D, A);
 
-    // ダブリングを使ってK回の遷移先を求める
-    ll now = 0;  // 開始の頂点番号
-    for(ll d=D-1; d>=0; d--) {
-        if ((K>>d)&1) {
-            now = doubling[d][now];
-        }
-    }
-    cout << now << endl;  // K回の遷移後の頂点番号
+    // 頂点0からK回辿った先の頂点は？
+    cout << dbl.query(0, K) << endl;
     // 3
 }
 
